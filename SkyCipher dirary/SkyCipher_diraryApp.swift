7f2984a16ea8acd1401529534c8b1da0c103d0d2
sky43535 <skylerp530@gmail.com>
2025-07-29 02:20:23 -0500

    Initial Commit

---
+++ SkyCipher dirary/SkyCipher_diraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCipher_diraryApp.swift
+//  SkyCipher dirary
+//
+//  Created by Owner on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SkyCipher_diraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
